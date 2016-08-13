edc5da691a25cc55731ad39076793a9f9a8313a4
Onni Hakala <onni@keksi.io>
2016-08-13 10:40:05 +0300

    Refactor and rename

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Must Use Options
- * Description: Plugin which adds forced options through WP_MUST_USE_OPTIONS constant
+ * Plugin Name: WP Readonly Options
+ * Description: Plugin which adds forced options through WP_READONLY_OPTIONS constant
  * Version: 1.0.0
  * Plugin URI: https://github.com/devgeniem/wp-must-use-options
  * Author: Onni Hakala / Geniem Oy
@@ -9,53 +9,74 @@
  * License: MIT License
  */
 
-namespace Geniem\helper;
+namespace Geniem\Helper;
 
-class MustUseOptions {
+class ReadonlyOptions {
 
-  /**
-   * Forces options from WP_MUST_USE_OPTIONS array to be predetermined
-   * This is useful if some plugin doesn't allow defining options in wp-config.php
-   *
-   * @param $options array - List of forced options
-   */
-  static function set( $options ) {
+    /**
+     * Helper text which is set to all readonly element titles by javascript
+     */
+    static $hover_text = '';
 
-    if ( is_array( $options ) ) {
+    /**
+     * Array off all $options. This is used to feed the options to javascript hack
+     */
+    static $options = [];
 
-        // Force mentioned options with filters
-        foreach ( $options as $must_use_option => $must_use_value ) {
+    /**
+     * Setup hooks, filters and default options
+     */
+    static function init() {
+        self::$hover_text = 'This option is set readonly in ' . basename(__DIR__).'/'.basename(__FILE__);
 
-            // Always return this value for the option
-            add_filter( "pre_option_{$must_use_option}", function() use ( $must_use_value ) {
-                return $must_use_value;
-            });
-
-            // Always deny saving this value to the DB
-            // wp-includes/option.php:280-291 stops updating this option if it's same
-            add_filter( "pre_update_option_{$must_use_option}", function() use ( $must_use_value ) {
-                return $must_use_value;
-            });
-
-            add_filter( "pre_update_option_{$must_use_option}", function() use ( $must_use_value ) {
-                return $must_use_value;
-            });
+        if ( is_admin() && ! defined('WP_READONLY_OPTIONS_NO_JS') ) {
+            add_action('admin_enqueue_scripts', [ __CLASS__, 'set_admin_readonly_js' ] );
         }
-
-        if ( is_admin() && ! defined('WP_MUST_USE_OPTIONS_NO_JS') ) {
-            self::admin_readonly_js( array_keys( $options ) );
-        }
-
     }
-  }
 
-  /**
-   * Set option input fields as readonly in admin pages so that users won't get confused
-   *
-   * @param array $input_element_ids - List of elements to set as readonly
-   */
-  static private function admin_readonly_js( $input_element_ids ) {
-    add_action('admin_enqueue_scripts', function( $page_name ) use ( $input_element_ids ) {
+    /**
+     * Helper to collect options from multiple uses of self::set()
+     *
+     * @param array $options - Options to be added
+     */
+    static function add_options(array $options) {
+        self::$options = array_merge($options,self::$options);
+    }
+
+    /**
+     * Forces options from WP_READONLY_OPTIONS array to be predetermined
+     * This is useful if some plugin doesn't allow defining options in wp-config.php
+     *
+     * @param $options array - List of forced options
+     */
+    static function set( $options ) {
+
+        if ( is_array( $options ) ) {
+
+            // Force mentioned options with filters
+            foreach ( $options as $must_use_option => $must_use_value ) {
+
+                // Always return this value for the option
+                add_filter( "pre_option_{$must_use_option}", function() use ( $must_use_value ) {
+                    return $must_use_value;
+                });
+
+                // Always deny saving this value to the DB
+                // wp-includes/option.php:280-291 stops updating this option if it's same
+                add_filter( "pre_update_option_{$must_use_option}", function() use ( $must_use_value ) {
+                    return $must_use_value;
+                });
+            }
+
+            // Add to bigger array which is used later on in admin_footer
+            self::add_options($options);
+        }
+    }
+
+    /**
+     * Set option input fields as readonly in admin pages so that users won't get confused
+     */
+    static function set_admin_readonly_js($page_name) {
         switch ($page_name) {
 
             // Enable readonly js fixer for all admin options pages
@@ -67,10 +88,12 @@ class MustUseOptions {
             || 'options-permalink.php'
             || 'options.php':
 
+            $hover_text = self::$hover_text;
+            $input_element_ids = array_keys( self::$options );
+
             // Add javascript which turns $elements into readonly
-            add_action( 'admin_footer', function() use ( $input_element_ids ) {
+            add_action( 'admin_footer', function() use ( $input_element_ids, $hover_text ) {
                 // Show information about this plugin in element mouseover title for easier debugging
-                $plugin_file = basename(__DIR__).'/'.basename(__FILE__);
                 ?>
                 <script>
                     (function() {
@@ -79,7 +102,7 @@ class MustUseOptions {
                             var element =  document.getElementById(elementId);
                             if (typeof(element) != 'undefined' && element != null) {
                               element.readOnly = true;
-                              element.title = 'This option is forced in <?php echo $plugin_file;?>';
+                              element.title = '<?php echo $hover_text;?>';
                             }
                         });
                     })();
@@ -91,12 +114,14 @@ class MustUseOptions {
             default:
             return;
         }
-    });
-  }
+    }
 
 }
 
+// Setup hooks
+ReadonlyOptions::init();
+
 // Default variable which is always used
-if ( defined( 'WP_MUST_USE_OPTIONS' ) ) {
-    MustUseOptions::set( WP_MUST_USE_OPTIONS );
+if ( defined( 'WP_READONLY_OPTIONS' ) ) {
+    ReadonlyOptions::set( WP_READONLY_OPTIONS );
 }
